b6422c534e39201da5b8e8c668a963054f6c466d
mainishanhoon <NishanKashyap@hotmail.com>
2024-11-22 21:00:42 +0530

    added guest card

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,15 @@ import { Lights } from '@/components/Lights';
 import { Link as Scroll } from 'react-scroll/modules';
 
 import * as React from 'react';
+import GuestCard from '@/components/GuestCard';
 
 export default function Hero() {
   return (
     <>
       <header className="absolute top-0 h-[80px] bg-transparent"></header>
       <section className="flex h-dvh w-screen items-center justify-center bg-gradient-to-b">
-        <div className={'relative h-full w-full px-4 bg-grid-white/[0.03]'}>
-          <div
-            className={
-              'relative z-[1] flex h-full w-full animate-moveUp flex-col items-start justify-center sm:items-center'
-            }
-          >
+        <div className="relative h-full w-full px-4 bg-grid-white/[0.03]">
+          <div className="relative z-[1] flex h-full w-full animate-moveUp flex-col items-start justify-center sm:items-center">
             <div className="max-w-2xl text-center">
               <div className="flex justify-center">
                 <Image
@@ -45,7 +42,7 @@ export default function Hero() {
                 </p>
               </div>
             </div>
-            <div className="flex w-screen items-center justify-center">
+            <div className="flex w-full items-center justify-center">
               <div className="mt-8 grid gap-5 sm:grid-cols-2">
                 <Link href="/form">
                   <Button size="lg" variant="ringHover" className="text-white">
@@ -70,11 +67,7 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <div
-            className={
-              'absolute bottom-0 left-0 z-0 h-full w-full animate-appear opacity-0'
-            }
-          >
+          <div className="absolute bottom-0 left-0 z-0 h-full w-full animate-appear opacity-0">
             <Lights />
           </div>
         </div>
@@ -84,132 +77,39 @@ export default function Hero() {
         id="guests"
         className="flex min-h-dvh w-screen items-center justify-center bg-gradient-to-b"
       >
-        <div
-          className={'relative min-h-[100vh] w-full px-4 bg-grid-white/[0.03]'}
-        >
-          <p className="my-[50px] text-center text-6xl">Our Guests</p>
-          <div id="cards" className="m-auto flex w-[70vw] flex-wrap gap-[20px]">
-            {/* Card */}
-            <div className="m-auto mt-14 flex w-[85vw] flex-col items-center gap-[10px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
-              <div className="flex-col items-start px-4 pb-0 pt-7">
-                <p className="text-4xl font-bold uppercase">Vishwa Mohan</p>
-                <h4 className="text-large font-bold">Founder : Aimers.Ai</h4>
-                <small className="text-default-500"></small>
-              </div>
-              <div className="overflow-visible py-2">
-                <Image
-                  alt="Card background"
-                  className="mt-16 rounded-xl object-cover"
-                  src="/Vishwa.jpg"
-                  width={400}
-                  height={250}
-                />
-              </div>
-            </div>
-
-            {/* Card */}
-            <div className="m-auto mt-14 flex w-[85vw] flex-col items-center gap-[10px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
-              <div className="flex-col items-start px-4 pb-0 pt-7">
-                <p className="text-4xl font-bold uppercase">Crazy Deep</p>
-                <h4 className="text-large font-bold">Youtuber</h4>
-                <small className="text-default-500"></small>
-              </div>
-              <div className="overflow-visible py-2">
-                <Image
-                  alt="Card background"
-                  className="mt-16 rounded-xl bg-white object-cover"
-                  src="/Crazydeep.png"
-                  width={400}
-                  height={250}
-                />
-              </div>
-            </div>
-            {/* Card */}
-            <div className="m-auto mt-14 flex w-[85vw] flex-col items-center gap-[10px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
-              <div className="flex-col items-start px-4 pb-0 pt-7">
-                <p className="text-4xl font-bold uppercase">Jayesh Ranjan</p>
-                <h4 className="text-large font-bold">
-                  Businessmen and Founder Virality Studios
-                </h4>
-                <small className="text-default-500"></small>
-              </div>
-              <div className="overflow-visible py-2">
-                <Image
-                  alt="Card background"
-                  className="mt-16 rounded-xl object-cover"
-                  src="/Jayesh.png"
-                  width={400}
-                  height={250}
-                />
-              </div>
-            </div>
-
-            {/* Card */}
-            <div className="m-auto mt-14 flex w-[85vw] flex-col items-center gap-[10px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
-              <div className="flex-col items-start px-4 pb-0 pt-7">
-                <p className="text-4xl font-bold uppercase">Shivam Kakkar</p>
-                <h4 className="text-large font-bold">Actor</h4>
-                <small className="text-default-500">
-                  Shivam Kakar is an actor from New Delhi, India, known for his
-                  roles in Flames (2018), Indoo Ki Jawani (2020), and Aspirants
-                  (2021).
-                  <br />
-                  <b> Ad Gully Digixx&apos;22 Young Gun Award Winner</b> Shivam
-                  Kakkar won the Ad Gully Digixx&apos;22 Young Gun Award, which
-                  recognizes the best young individual in the agency and
-                  marketing fields between the ages of 24 and 29.
-                  <br />
-                  <b> Co-Founder of Xero Degrees Cafe Private Limited </b>Shivam
-                  Kakkar is the co-founder of Xero Degrees Cafe Private Limited.
-                </small>
-              </div>
-              <div className="overflow-visible py-2">
-                <Image
-                  alt="Card background"
-                  className="mt-2 rounded-xl object-cover"
-                  src="https://nextui.org/images/hero-card-complete.jpeg"
-                  width={400}
-                  height={250}
-                />
-              </div>
-            </div>
-            {/* Card */}
-            <div className="m-auto mt-14 flex w-[85vw] flex-col items-center gap-[10px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
-              <div className="flex-col items-start px-4 pb-0 pt-7">
-                <p className="text-4xl font-bold uppercase">Samrat Bhai</p>
-                <h4 className="text-large font-bold">Actor</h4>
-                <small className="text-default-500"></small>
-              </div>
-              <div className="overflow-visible py-2">
-                <Image
-                  alt="Card background"
-                  className="mt-16 rounded-xl object-cover"
-                  src="https://nextui.org/images/hero-card-complete.jpeg"
-                  width={400}
-                  height={400}
-                />
-              </div>
-            </div>
-
-            {/* Card */}
-            <div className="m-auto mt-14 flex w-[85vw] flex-col items-center gap-[10px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
-              <div className="flex-col items-start px-4 pb-0 pt-7">
-                <p className="text-4xl font-bold uppercase">Tanya Mittal</p>
-                <h4 className="text-large font-bold">
-                  Businesswomen and Influncer with 1.2M followers
-                </h4>
-                <small className="text-default-500"></small>
-              </div>
-              <div className="overflow-visible py-2">
-                <Image
-                  alt="Card background"
-                  className="mt-16 rounded-xl object-cover"
-                  src="https://nextui.org/images/hero-card-complete.jpeg"
-                  width={400}
-                  height={250}
-                />
-              </div>
-            </div>
+        <div className="relative h-full w-full bg-grid-white/[0.03]">
+          <p className="pt-10 text-center text-6xl font-bold">Our Guests</p>
+          <div id="cards" className="grid lg:grid-cols-2 xl:grid-cols-3 px-4">
+            <GuestCard
+              name="CrazyDeep"
+              designation="Youtuber"
+              description="YouTube Channel"
+              imageSrc="/Crazydeep.webp"
+            />
+            <GuestCard
+              name="Samrat Bhai"
+              designation="Youtuber"
+              description="YouTube channel"
+              imageSrc="/Samrat.webp"
+            />
+            <GuestCard
+              name="Vishwa Mohan Singh"
+              designation="Youtuber"
+              description="YouTube channel"
+              imageSrc="/Vishwa.webp"
+            />
+            <GuestCard
+              name="Jayesh Ranjan"
+              designation="Youtuber"
+              description="YouTube channel"
+              imageSrc="/Jayesh.webp"
+            />
+            <GuestCard
+              name="Prabal Bhatt"
+              designation="Youtuber"
+              description="YouTube channel"
+              imageSrc="/Prabal.webp"
+            />
           </div>
         </div>
       </section>
--- components/GuestCard.tsx
+++ components/GuestCard.tsx
@@ -1,21 +1,38 @@
 import Image from 'next/image';
 
-export default function GuestCard() {
+interface GuestCardProps {
+  imageSrc: string;
+  name: string;
+  designation: string;
+  description: string;
+  altText?: string;
+}
+
+export default function GuestCard({
+  imageSrc,
+  name,
+  designation,
+  description,
+  altText,
+}: GuestCardProps) {
   return (
-    <div className="hofrom-blue-500 m-auto mt-14 flex w-[85vw] flex-col items-center gap-[50px] rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-4 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:to-purple-700 md:w-[450px]">
-      <div className="flex-col items-start px-4 pb-0 pt-7">
-        <p className="text-4xl font-bold uppercase">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="text-large font-bold">Frontend Radio</h4>
-      </div>
+    <div className="mx-auto mt-10 flex flex-col gap-2 rounded-3xl border-2 border-slate-500 bg-gradient-to-br from-[#6dbbff75] to-[#6dbbff05] p-5 shadow-[0_1px_30px_rgba(98,182,255,0.4)] hover:from-blue-500 hover:to-purple-700 md:w-[450px]">
       <div className="overflow-visible py-2">
         <Image
-          alt="Guest Card"
-          className="mt-16 rounded-xl object-cover"
-          src="https://nextui.org/images/hero-card-complete.jpeg"
+          alt={altText || `${name}'s profile picture`}
+          className="rounded-xl bg-white object-cover"
+          src={imageSrc}
+          loading="lazy"
           width={400}
           height={250}
         />
+      </div>
+      <div className="flex-col">
+        <h2 className="text-2xl font-bold uppercase tracking-wider sm:text-3xl">
+          {name}
+        </h2>
+        <h3 className="text-xl font-bold tracking-wider">{designation}</h3>
+        <p className="text-default-500 tracking-wider">{description}</p>
       </div>
     </div>
   );
